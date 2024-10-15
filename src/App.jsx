6e9dd7cba0e66f705880769612c64a32200d2d14
Felipech292 <felipechicaiza47@gmail.com>
2024-10-14 23:18:45 -0400

    Finished Layout & Corrected App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,106 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Gallery from './components/Gallery'
+const ACCESS_KEY = import.meta.env.VITE_ACCESS_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [banList, setBanList] = useState([])
+  const [currentArt, setCurrentArt] = useState(null);
+  const [currentImage, setCurrentImage] = useState(null);
+  const [prevImages, setPrevImages] = useState([]);
+
+  const discoverForm = () => {
+    let discovered =  false;
+
+    makeQuery();
+    console.log("ran");
+    console.log(currentImage);
+  }
+
+  const addBanItem = (art, key) => {
+    setBanList([...banList, art[key]]);
+    console.log(banList);
+    makeQuery();
+  }
+
+  const makeQuery = () => {
+    let query = `https://api.harvardartmuseums.org/object?apikey=${ACCESS_KEY}&size=1&sort=random&hasimage=1`;
+    callAPI(query).catch(consol.error);
+  }
+
+  const callAPI = async (query) => {
+    const response = await fetch(query);
+    const json = await response.json();
+    setCurrentArt(json.records[0])
+    imageQuery(json.records[0].id)
+  }
+
+  const imageQuery = (imageId) => {
+    let query = `https://api.harvardartmuseums.org/image?apikey=${ACCESS_KEY}&id=${imageId}`;
+    callImageAPI(query).catch(console.error);
+  }
+
+  const callImageAPI = async (query) => {
+    const response = await fetch(query);
+    const json = await response.json();
+    console.log(json);
+    if (json.records.length!=0)
+    {
+      setCurrentImage(json.records[0].baseimageurl);
+      setPrevImages((images) => [...images, json.records[0].baseimageurl]);
+      console.log("correct")
+    }
+    else{
+      setCurrentImage();
+    }
+  }
+
+  const buttons = banList.map((d)=>{
+    return <button className='banned-buttons' key={d}>{d}</button>;
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <h1>Discover New Art! 🎨</h1>
+      <button onClick={discoverForm}>🔎 Discover</button>
+      {currentArt ? (<div className='art-description'><h2>{currentArt.title}</h2>
+      {currentArt.technique ? (<button className='attribute-buttons' onClick={() =>{addBanItem(currentArt,"technique")}}>{currentArt.technique}</button> ): (<div></div>)}
+      {currentArt.century ? (<button className='attribute-buttons' onClick={() =>{addBanItem(currentArt,"century")}}>{currentArt.century}</button> ): (<div></div>)} 
+      {currentArt.culture ? (<button className='attribute-buttons' onClick={() =>{addBanItem(currentArt,"culture")}}>{currentArt.culture}</button> ): (<div></div>)}
+      {currentArt.classification ? (<button className='attribute-buttons' onClick={() =>{addBanItem(currentArt,"classification")}}>{currentArt.classification}</button> ): (<div></div>)} 
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      ) : (
+        <div> </div>)}
+
+        <br></br>
+        <br></br>
+
+        {currentImage ? (
+           <img
+           className="screenshot"
+           src={currentImage}
+           alt="Screenshot returned"
+           height="500"
+           width ="700"
+         />
+       ) : (
+         <div> </div>
+       )}
+
+       <br></br>
+
+       <div className='container'>
+        <Gallery images={prevImages} />
+       </div>
+
+       <div className='ban-list'>
+        <h3> Banned Attributes 🚫</h3>
+        {buttons}
+       </div>
+
+
+    </div>
   )
 }
 
